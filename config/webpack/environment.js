aa3c1dabde4b44610554506c6ae0de1eaf1b306c
Alex Tsui <alextsui@pm.me>
2020-06-30 21:52:03 +0000

    :ok_hand: Configure webpack

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,39 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.merge({
+  output: {
+    filename: '[name].[contenthash:8].js',
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        // @see https://hackernoon.com/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+            return `npm.${packageName.replace('@', '')}`;
+          },
+          priority: 10,
+        },
+        // libs: {
+        //   test: /app\/frontend\/src\/javascripts\/libs/,
+        //   name: 'libs',
+        //   priority: 1,
+        // },
+        // 'css-vendor': {
+        //   test: /app\/frontend\/src\/stylesheets\/vendor\.scss/,
+        //   name: "css-vendor",
+        // }
+      },
+    }
+  },
+  devtool: false,
+})
+
 module.exports = environment
